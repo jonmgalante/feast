e9b8653afdb639f21201e501bba7c5bd7964b98e
Jon Galante <jonmgalante@gmail.com>
2026-03-22 12:54:33 -0400

    Initial Commit

---
+++ Feast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Feast
+//
+//  Created by Jon Galante on 3/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Feast/FeastApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeastApp.swift
+//  Feast
+//
+//  Created by Jon Galante on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct FeastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
